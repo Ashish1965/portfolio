d28c13c27408ea4fc48effa6681271c7e0597ea8
Ashish1965 <121724473+Ashish1965@users.noreply.github.com>
2024-12-16 15:58:20 +0530

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -1,74 +1,252 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { useRouter } from "next/router";
+import { useTheme } from "@/context/ThemeContext";
 import ThemeToggle from "./ThememToggle";
-const Main = () => {
-  return (
-    <div
-      id="home"
-      className="w-full text-center mt-40 lg:mt-12"
-    >
-      {/* <div className="absolute flex items-end justify-end mt-14 ml-5">
-      <ThemeToggle />
-      </div> */}
-      <div className="fixed top-24 right-8 z-10">
-      <ThemeToggle />
-      </div>
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div className="flex flex-col items-center justify-center">
-          <img
-            src="/assets/promo.jpeg"
-            alt=""
-            className="md:mt-36 rounded-[100%] h-64 w-64"
-          />
-          <p className="mt-4 uppercase text-sm tracking-widest text-gray-700 dark:text-gray-600">
-            LET'S BUILD SOMETHING TOGETHER
-          </p>
-          <h1 className="py-4 text-gray-700 dark:text-gray-600">
-            Hi, I'm <span className="text-[#5651e5]">Ashish</span>
-          </h1>
-          <h1 className="py-2 text-gray-700 dark:text-gray-600">
-            A Full-Stack Web Developer
-          </h1>
-          <p className="py-4 text-gray-700 max-w-[70%] mx-auto dark:text-gray-500">
-            A passionate Full Stack Web Developer dedicated to crafting dynamic
-            and user-centric digital experiences. With a blend of creativity and
-            technical expertise, I thrive on bringing ideas to life through
-            innovative web solutions.I'm on a perpetual journey of exploring the
-            realms of web development, constantly seeking to merge the artistry
-            of design with the precision of code. My love for problem-solving
-            and the ever-evolving nature of technology fuels my enthusiasm to
-            create robust and elegant web applications.
-          </p>
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-            <Link href="https://www.linkedin.com/in/ashish-narawariya-7536b6242/">
-              <div className="rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-700 p-6 cursor-pointer hover-scale-effect ">
-                <FaLinkedin className="dark:bg-gray-500" />
-              </div>
-            </Link>
-            <Link href="https://github.com/Ashish1965/">
-              <div className="rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-700 p-6 cursor-pointer hover-scale-effect">
-                <FaGithub className="dark:bg-gray-500" />
-              </div>
-            </Link>
 
-            <Link href="https://www.instagram.com/ashishnarawariya/">
-              <div className="rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-700 p-6 cursor-pointer hover-scale-effect">
-                <FaInstagram className="dark:bg-gray-500" />
-              </div>
-            </Link>
-            <Link href="https://x.com/AshishNara33521/">
-              <div className="rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-700 p-6 cursor-pointer hover-scale-effect">
-                <FaXTwitter className="dark:bg-gray-500" />
-              </div>
-            </Link>
-          </div>
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState("#ecf0f3");
+  const [linkColor, setLinkColor] = useState("#1f2937");
+  const router = useRouter();
+  const {theme} = useTheme();
+  // console.log(theme);
+
+  
+
+  useEffect(() => {
+    if (
+      router.asPath === "/elegant" ||
+      router.asPath === "/school" ||
+      router.asPath === "/bookmark" ||
+      router.asPath === "/score" ||
+      router.asPath === "/netflix"
+    ) {
+      setNavBg("transparent");
+      // console.log("router");
+      setLinkColor("#ecf0f3");
+    }
+    
+    else {
+      if(theme === 'light'){
+        // console.log('light');
+
+        setNavBg("#ecf0f3");
+      }
+      // setNavBg("#ecf0f3");
+      // setLinkColor("#1f2937");
+    }
+  }, [router]);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener("scroll", handleShadow);
+  }, []);
+
+  return (
+    <div>
+      <div
+        style={{ backgroundColor: `${navBg}` }}
+        className={
+          shadow
+            ? "fixed w-full h-20 shadow-xl z-[100]"
+            : "fixed w-full h-20 z-[100]"
+        }
+      >
+        <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16 dark:bg-gray-900">
+          <div>
+          <Link href="/">
+            <Image src="/assets/logo.png" alt="Logo" width="90" height="35" />
+          </Link>
           
+          </div>
+          <div>
+            <ul style={{ color: `${linkColor}` }} className="hidden md:flex">
+              <Link href="/#home">
+                <li className="ml-10 text-sm uppercase hover:border-h dark:text-gray-500">Home</li>
+              </Link>
+              <Link href="/#education">
+                <li className="ml-10 text-sm uppercase hover:border-h dark:text-gray-500">
+                  Education
+                </li>
+              </Link>
+              <Link href="/#about">
+                <li className="ml-10 text-sm uppercase hover:border-h dark:text-gray-500">
+                  About
+                </li>
+              </Link>
+              <Link href="/#skills">
+                <li className="ml-10 text-sm uppercase hover:border-h dark:text-gray-500">
+                  Skills
+                </li>
+              </Link>
+              <Link href="/#projects">
+                <li className="ml-10 text-sm uppercase hover:border-h dark:text-gray-500">
+                  Projects
+                </li>
+              </Link>
+              <Link href="/#contact">
+                <li className="ml-10 text-sm uppercase hover:border-h dark:text-gray-500">
+                  Contact
+                </li>
+              </Link>
+            </ul>
+            <div onClick={handleNav} className="md:hidden">
+              <AiOutlineMenu className="dark:text-gray-500 text-gray-400" />
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            nav
+              ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70"
+              : ""
+          }
+        >
+          <div
+            className={
+              nav
+                ? "fixed left-0 top-0 w-[85%] md:w-[45%] h-screen bg-white p-10 nav-transition dark:bg-gray-900"
+                : "fixed left-[-100%] top-0 w-[75%] md:w-[45%] h-screen bg-white dark:bg-gray-900  p-10 nav-transition "
+            }
+          >
+            <div>
+              <div className="flex w-full items-center justify-between">
+                <Link href="/">
+                  <Image
+                    src="/assets/logo.png"
+                    height="100"
+                    width="50"
+                    alt="logo"
+                  />
+                </Link>
+                <div
+                  onClick={handleNav}
+                  className="rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-700 dark:text-gray-500 p-3 cursor-pointer"
+                >
+                  <AiOutlineClose />
+                </div>
+              </div>
+              <div className="border-b border-gray-300 my-4">
+                <p className="w-[85%] md:w-[90%] py-4 dark:text-gray-500">
+                  Let's build something legendary together
+                </p>
+              </div>
+            </div>
+            <div className="py-4 flex flex-col">
+              <ul className="uppercase">
+                <Link href="/">
+                  <li
+                    onClick={() => {
+                      setNav(false);
+                    }}
+                    className="py-4 text-sm dark:text-gray-500"
+                  >
+                    Home
+                  </li>
+                </Link>
+                <Link href="/#education">
+                  <li
+                    onClick={() => {
+                      setNav(false);
+                    }}
+                    className="py-4 text-sm dark:text-gray-500"
+                  >
+                    Education
+                  </li>
+                </Link>
+                <Link href="/#about">
+                  <li
+                    onClick={() => {
+                      setNav(false);
+                    }}
+                    className="py-4 text-sm dark:text-gray-500"
+                  >
+                    About
+                  </li>
+                </Link>
+                <Link href="/#skills">
+                  <li
+                    onClick={() => {
+                      setNav(false);
+                    }}
+                    className="py-4 text-sm dark:text-gray-500"
+                  >
+                    Skills
+                  </li>
+                </Link>
+                <Link href="/#projects">
+                  <li
+                    onClick={() => {
+                      setNav(false);
+                    }}
+                    className="py-4 text-sm dark:text-gray-500"
+                  >
+                    Projects
+                  </li>
+                </Link>
+                <Link href="/#contact">
+                  <li
+                    onClick={() => {
+                      setNav(false);
+                    }}
+                    className="py-4 text-sm dark:text-gray-500"
+                  >
+                    Contact
+                  </li>
+                </Link>
+              </ul>
+              <div className="pt-4">
+                <p className="uppercase tracking-widest text-blue-500">
+                  Let's Connect
+                </p>
+                <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                  <Link href="https://www.linkedin.com/in/ashish-narawariya-7536b6242/">
+                    <div className="rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-700 p-6 cursor-pointer hover-scale-effect">
+                      <FaLinkedin className="dark:bg-gray-500" />
+                    </div>
+                  </Link>
+                  <Link href="https://github.com/Ashish1965/">
+                    <div className="rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-700 p-6 cursor-pointer hover-scale-effect">
+                      <FaGithub className="dark:bg-gray-500" />
+                    </div>
+                  </Link>
+
+                  <Link href="https://www.instagram.com/ashishnarawariya/">
+                    <div className="rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-700 p-6 cursor-pointer hover-scale-effect">
+                      <FaInstagram className="dark:bg-gray-500" />
+                    </div>
+                  </Link>
+                  <Link href="https://x.com/AshishNara33521/">
+                    <div className="rounded-full shadow-lg shadow-gray-400 dark:shadow-gray-700 p-6 cursor-pointer hover-scale-effect">
+                      <FaXTwitter className="dark:bg-gray-500" />
+                    </div>
+                  </Link>
+                </div>
+
+                
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Main;
+export default Navbar;
