3d51d58bada6ec7fc5a0a230304054879bf3c78d
Ashish1965 <121724473+Ashish1965@users.noreply.github.com>
2024-12-16 11:15:31 +0530

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -71,7 +71,7 @@ const Contact = () => {
   }
 
   return (
-    <div id="contact" className="w-full dark:bg-gray-800">
+    <div id="contact" className="w-full">
       <div className="max-w-[1240px] m-auto px-2 py-8 w-full  ">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Contact
